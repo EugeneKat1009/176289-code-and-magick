e064d980493148fae6f58602215c17679bc7b4c6
EugeneKat1009 <zhenkatusha@gmail.com>
2018-04-06 17:39:23 +0300

    задание 3 с правками

--- js/setup.js
+++ js/setup.js
@@ -1,63 +1,58 @@
 'use strict';
 
 var WIZARD_NAMES = [
-'Иван',
-'Хуан Себастьян',
-'Мария',
-'Кристоф',
-'Виктор',
-'Юлия',
-'Люпита',
-'Вашингтон'
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
 ];
 
 var WIZARD_SECOND_NAMES = [
-'да Марья',
-'Верон',
-'Мирабелла',
-'Вальц',
-'Онопко',
-'Топольницкая',
-'Нионго',
-'Ирвинг'
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
 ];
 
 var WIZARD_COAT_COLOR = [
-'rgb(101, 137, 164)',
-'rgb(241, 43, 107)',
-'rgb(146, 100, 161)',
-'rgb(56, 159, 117)',
-'rgb(215, 210, 55)',
-'rgb(0, 0, 0)'
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
 ];
 
 var WIZARD_EYES_COLOR = [
-'black',
-'red',
-'blue',
-'yellow',
-'green'
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
 ];
 
 var popup = document.querySelector('.setup');
 popup.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
-var closePopup = document.querySelector('.setup-close');
-closePopup.addEventListener('click', function (evt) {
-popup.classList.add('hidden');
-});
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content.querySelector('.setup-similar-item');
+    .content.querySelector('.setup-similar-item');
 
 var getRandom = function (arr) {
   var rand = Math.floor(Math.random() * arr);
   return rand;
 };
 
-var wizards = function () {
+var Wizards = function () {
   this.name = WIZARD_NAMES[getRandom(WIZARD_NAMES.length)] + ' ' + WIZARD_SECOND_NAMES[getRandom(WIZARD_SECOND_NAMES.length)];
   this.coatColor = WIZARD_COAT_COLOR[getRandom(WIZARD_COAT_COLOR.length)];
   this.eyesColor = WIZARD_EYES_COLOR[getRandom(WIZARD_EYES_COLOR.length)];
@@ -67,7 +62,7 @@ var WIZARDS_COUNT = 4;
 var similarWizards = [];
 
 for (var j = 0; j < WIZARDS_COUNT; j++) {
-  similarWizards.push(new wizards());
+  similarWizards.push(new Wizards());
 }
 
 var renderWizard = function (wizard) {
@@ -78,7 +73,7 @@ var renderWizard = function (wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < WIZARDS_COUNT; i++) {
