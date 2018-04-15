6fe01413f82e0ead2609ae09680427e9d9139b77
EugeneKat1009 <zhenkatusha@gmail.com>
2018-04-15 14:48:57 +0300

    без сброса положения попапа

--- js/setup.js
+++ js/setup.js
@@ -39,8 +39,17 @@ var WIZARD_EYES_COLOR = [
   'green'
 ];
 
-var popup = document.querySelector('.setup');
-popup.classList.remove('hidden');
+var WIZARD_FIREBALL_COLOR = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+var ESC_BTN = 27;
+var ENTER_BTN = 13;
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -80,3 +89,125 @@ for (var i = 0; i < WIZARDS_COUNT; i++) {
   fragment.appendChild(renderWizard(similarWizards[i]));
 }
 similarListElement.appendChild(fragment);
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_BTN) {
+    popupClose();
+  }
+};
+
+var popupOpen = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var popupClose = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  popupOpen();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_BTN) {
+    popupOpen();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  popupClose();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_BTN) {
+    popupClose();
+  }
+});
+
+var setupWizard = document.querySelector('.setup-wizard');
+var setupFireball = document.querySelector('.setup-fireball-wrap');
+var inputFireball = setupFireball.querySelector('input');
+
+var onWizzardClick = function () {
+  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+  wizardEyes.style.fill = WIZARD_EYES_COLOR[getRandom(WIZARD_EYES_COLOR.length)];
+  var onInputEyesColor = document.getElementsByName('eyes-color');
+  onInputEyesColor[0].value = wizardEyes.style.fill;
+};
+
+var onFireballClick = function () {
+  var newFireballColor = WIZARD_FIREBALL_COLOR[getRandom(WIZARD_FIREBALL_COLOR.length)];
+  setupFireball.style.backgroundColor = newFireballColor;
+  inputFireball.value = newFireballColor;
+};
+
+setupWizard.addEventListener('click', onWizzardClick);
+setupWizard.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_BTN) {
+    onWizzardClick();
+  }
+});
+
+setupFireball.addEventListener('click', onFireballClick);
+setupFireball.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_BTN) {
+    onFireballClick();
+  }
+});
+
+var shopElement = document.querySelector('.setup-artifacts-shop');
+var draggedItem = null;
+var artifatsCells = document.querySelectorAll('.setup-artifacts .setup-artifacts-cell');
+
+var showEmptyCells = function () {
+  Array.prototype.forEach.call(artifatsCells, function (cell) {
+    if (!cell.hasChildNodes()) {
+      cell.style.outline = '2px dashed red';
+    }
+  });
+};
+
+var hideEmptyCells = function () {
+  Array.prototype.forEach.call(artifatsCells, function (cell) {
+    cell.style.outline = '';
+  });
+};
+
+shopElement.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+    showEmptyCells();
+  }
+});
+
+var artifactsElement = document.querySelector('.setup-artifacts');
+
+artifactsElement.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return false;
+});
+
+artifactsElement.addEventListener('drop', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.target.appendChild(draggedItem);
+  hideEmptyCells();
+  evt.preventDefault();
+});
+
+
+artifactsElement.addEventListener('dragenter', function (evt) {
+  evt.target.style.backgroundColor = 'yellow';
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragleave', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.preventDefault();
+});
